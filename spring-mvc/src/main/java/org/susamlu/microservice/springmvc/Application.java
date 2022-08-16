442694d96b257dbe5093c7bf872d5251a0d5d6b3
Sam Lu <luxiaocong524@163.com>
2022-08-17 00:19:07 +0800

    feat: 修改 spring-mvc 启动类

--- spring-mvc/src/main/java/org/susamlu/microservice/springmvc/Application.java
+++ spring-mvc/src/main/java/org/susamlu/microservice/springmvc/Application.java
@@ -2,16 +2,12 @@ package org.susamlu.microservice.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Sam Lu
  * @date 2022/08/16
  */
 @SpringBootApplication
-@RestController
 public class Application {
 
     public static void main(String[] args) {
