c2a63e912e4afd4365a4f669ca5564a0af70ef05
Sam Lu <luxiaocong524@163.com>
2022-08-18 23:17:13 +0800

    feat: 修改 spring-mvc-validation，提交 PatternAnnotationRequest 示例代码

---
+++ spring-mvc-validation/src/main/java/org/susamlu/springmvc/controller/model/PatternAnnotationRequest.java
@@ -0,0 +1,17 @@
+package org.susamlu.springmvc.controller.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.Pattern;
+
+/**
+ * @author Sam Lu
+ * @date 2022/08/18
+ */
+@Data
+public class PatternAnnotationRequest {
+
+    @Pattern(regexp = "^1[3456789]\\d{9}$", message = "手机号码格式不正确")
+    private String mobile;
+
+}
