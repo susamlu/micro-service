3bd5d086cbcce7c45fbfe52ac824f9901731fcae
Sam Lu <luxiaocong524@163.com>
2022-09-22 13:00:26 +0800

    feat: 提交 spring-web-helloworld-explore

---
+++ spring-web-helloworld-explore/src/main/java/org/susamlu/springweb/explore/HelloWorldExploreApplication.java
@@ -0,0 +1,17 @@
+package org.susamlu.springweb.explore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sam Lu
+ * @date 2022/08/16
+ */
+@SpringBootApplication
+public class HelloWorldExploreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldExploreApplication.class, args);
+    }
+
+}
