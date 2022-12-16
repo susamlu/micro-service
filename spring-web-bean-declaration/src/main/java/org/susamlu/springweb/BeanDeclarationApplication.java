c4f63590136659edb20fb088b30fd44294832913
Sam Lu <luxiaocong524@163.com>
2022-12-16 12:55:37 +0800

    feat: 提交 spring-web-bean-declaration

--- spring-web-bean-declaration/src/main/java/org/susamlu/springweb/BeanDeclarationApplication.java
+++ spring-web-bean-declaration/src/main/java/org/susamlu/springweb/BeanDeclarationApplication.java
@@ -6,26 +6,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author Sam Lu
  * @date 2022/08/16
  */
 @SpringBootApplication
-public class BeanApplication implements ApplicationContextAware {
+public class BeanDeclarationApplication implements ApplicationContextAware {
 
     @Autowired
     private static ApplicationContext applicationContext;
 
     public static void main(String[] args) {
-        SpringApplication.run(BeanApplication.class, args);
+        SpringApplication.run(BeanDeclarationApplication.class, args);
         // ((ConfigurableApplicationContext) applicationContext).close();
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        BeanApplication.applicationContext = applicationContext;
+        BeanDeclarationApplication.applicationContext = applicationContext;
     }
 
 }
