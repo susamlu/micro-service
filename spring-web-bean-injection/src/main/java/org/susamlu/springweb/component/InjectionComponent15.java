6f2a69fae11ac148b208f35d500083892c669346
Sam Lu <luxiaocong524@163.com>
2022-12-20 09:13:23 +0800

    feat: 修改 spring-web-bean-injection

---
+++ spring-web-bean-injection/src/main/java/org/susamlu/springweb/component/InjectionComponent15.java
@@ -0,0 +1,31 @@
+package org.susamlu.springweb.component;
+
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+
+/**
+ * @author Sam Lu
+ * @date 2022/12/20
+ */
+@Component
+public class InjectionComponent15 {
+
+    @Inject
+    private MyBean myBean1;
+
+    private MyBean myBean2;
+
+    @Inject
+    public void setMyBean(MyBean myBean) {
+        this.myBean2 = myBean;
+    }
+
+    private MyBean myBean3;
+
+    @Inject
+    public InjectionComponent15(MyBean myBean) {
+        this.myBean3 = myBean;
+    }
+
+}
---
+++ spring-web-bean-injection/src/main/java/org/susamlu/springweb/component/InjectionComponent16.java
@@ -0,0 +1,31 @@
+package org.susamlu.springweb.component;
+
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+
+/**
+ * @author Sam Lu
+ * @date 2022/12/20
+ */
+@Component
+public class InjectionComponent16 {
+
+    @Inject
+    private ResourceBean resourceBean1;
+
+    private ResourceBean resourceBean2;
+
+    @Inject
+    public void setResourceBean(ResourceBean resourceBean2) {
+        this.resourceBean2 = resourceBean2;
+    }
+
+    private ResourceBean resourceBean3;
+
+    @Inject
+    public InjectionComponent16(ResourceBean resourceBean2) {
+        this.resourceBean3 = resourceBean2;
+    }
+
+}
